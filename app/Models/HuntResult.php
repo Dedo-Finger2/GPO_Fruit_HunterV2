7f8a402617581337815d8fe24d941f47d74a51cc
Dedo_Finger2 <antonioimportant@gmail.com>
2023-07-27 21:47:04 -0300

    controllers and models created

---
+++ app/Models/HuntResult.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HuntResult extends Model
+{
+    use HasFactory;
+
+    protected $table = "hunt_results";
+
+}
